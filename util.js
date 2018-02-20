612c99a95ae9d69723a206a5775ee694115f046e
Roman Kuzmichev <Daerdemandt@gmail.com>
2018-02-20 20:21:58 +0300

    Fix issue #2 (inheritance form DispatchClass)

--- util.js
+++ util.js
@@ -26,6 +26,12 @@ class EventEmitterPromiseAllEmit extends EventEmitter {
 	}
 }
 
+const getClassProtoChain = (cls) => {
+	if (!cls.prototype) return [];
+	const result = getClassProtoChain(Object.getPrototypeOf(cls));
+	result.unshift(cls.prototype);
+	return result;
+}
 
 // Basically cond, but with constructors instead of actions
 const DispatchClass = (pairs) => {
@@ -33,7 +39,24 @@ const DispatchClass = (pairs) => {
 	return class DispatchClass {
 		constructor(...data) {
 			for (const [predicate, cls] of pairs) {
-				if (predicate(...data)) return new cls(...data);
+				if (predicate(...data)) {
+					const result =  new cls(...data);
+					if (new.target != DispatchClass) { // we have an inheritance here
+						// We stich together stuff that inherited from DispatchClass and our cls
+						// Note that we're changing child classes, the same child classes.
+						// If child class can make the DC resolve into different classes from time to time,
+						// unexpected and interesting things can happen, so don't do that.
+						// TODO: fix this, or at least throw an error when it happens
+						const protoChain = getClassProtoChain(new.target).slice(0, -1); // slice to omit DispatchClass
+						protoChain.push(cls.prototype);
+						let tmp = result;
+						for (const proto of protoChain) {
+							tmp.__proto__ = proto;
+							tmp = tmp.__proto__;
+						}
+					}
+					return result;
+				}
 			}
 			throw new Error('No predicate matched the data');
 		}
